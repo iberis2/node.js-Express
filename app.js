2a134ac86195dab35b74586ae60eee5140e68da5
iberis2 <snow9402@gmail.com>
2023-02-07 09:49:45 +0900

    req.query로 특정 정보 조회

--- app.js
+++ app.js
@@ -4,14 +4,20 @@ const app = express();
 const members = require("./members");
 
 app.get("/api/members", (req, res) => {
-  res.send(members);
+  const { team } = req.query;
+  if (team) {
+    const teamMember = members.filter((m) => m.team === team);
+    res.status(201).send(teamMember);
+  } else {
+    res.status(201).send(members);
+  }
 });
 
 app.get("/api/members/:id", (req, res) => {
   const { id } = req.params;
   const member = members.find((m) => m.id === Number(id));
   if (member) {
-    res.status(200).send(member);
+    res.status(201).send(member);
   } else {
     res.status(404).send({ message: `There's no such member` });
   }
